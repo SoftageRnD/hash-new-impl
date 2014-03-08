7291fc694ff5b7e14840a0822227065a63044795
Mike Sokolov <mikhail.g.sokolov@gmail.com>
2014-03-08 13:47:51 +0700

    Removed HashSetSuite as it was just a stub and didn't compile anyway

--- src/test/scala/ru/softage/collection/mutable/HashSetSuite.scala
+++
@@ -1,9 +0,0 @@
-package ru.softage.collection.mutable
-
-import org.scalatest.FunSuite
-
-class HashSetSuite extends FunSuite {
-  test("An empty Set should have size 0") {
-    assert(HashSet.empty.size == 0)
-  }
-}
